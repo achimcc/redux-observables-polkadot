0b1e59e720677b608a604806a60affa4caed953c
achimcc <achim@parity.com>
2021-04-24 22:36:24 +0200

    redux observables working

--- src/store/root.ts
+++ src/store/root.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { combineEpics } from 'redux-observable';
-import {reducer} from './reducers';
+import { reducer } from './reducers';
+import { epic } from './epics'
 
 export const rootReducer = combineReducers({
     task: reducer
   });
 
   export const rootEpic = combineEpics(
+    epic
   );
