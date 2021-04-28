64f851b85845ba1cbbdc43c3fd8f28c1db883ec5
achimcc <achim@parity.com>
2021-04-28 18:22:54 +0200

    progress on contract deployment

--- src/epics/deploy.ts
+++ src/epics/deploy.ts
@@ -1,27 +1,36 @@
 import { ActionsObservable } from 'redux-observable';
 import { Action } from '../reducers/actions';
 import { filter, map, mergeMap } from 'rxjs/operators';
-import { ApiRx, Keyring } from '@polkadot/api';
-import { CodeRx } from '@polkadot/api-contract';
-import type { AnyJson } from '@polkadot/types/types';
+import { from, of } from 'rxjs';
+import { ApiRx, Keyring, ApiPromise } from '@polkadot/api';
+import { CodeRx, CodePromise } from '@polkadot/api-contract';
+import type { AnyJson, ISubmittableResult } from '@polkadot/types/types';
+import { SubmittableExtrinsic } from '@polkadot/api/types';
+import { promises } from 'node:dns';
 
 export const deploy = (action$: ActionsObservable<Action>, state$: any) =>
 	action$.ofType('Deploy').pipe(
-		map((action: Action) => {
+		mergeMap((action: Action) => {
 			const api = (state$ as any).contract.api as ApiRx;
+			const apiPromise = new ApiPromise();
 			const abi = (state$ as any).contract.abi as AnyJson;
 			const wasm = (state$ as any).contract.wasm as Uint8Array;
-			return new CodeRx(api, abi, wasm);
+			const code = new CodeRx(api, abi, wasm);
+			const blueprint = code.tx.createBlueprint({});
+			return blueprint;
 		}),
-		map((codeRx: CodeRx) => {
-			console.log('code: ', codeRx);
-			const bluePrint = codeRx.tx.createBlueprint;
-			console.log('blue', bluePrint);
+		map(blueprint => {
+			console.log('code: ', blueprint);
+			const keyring = new Keyring({ type: 'sr25519' });
+			const alice = keyring.addFromUri('//Alice');
+			console.log('blue', blueprint);
 			return { type: 'Any', payload: {} };
 		})
 	);
 
-/*mergeMap(submittable => {
+/*
+const result = blueprint.signAndSend();
+mergeMap(submittable => {
 			const keyring = new Keyring({ type: 'sr25519' });
 			const alice = keyring.addFromUri('//Alice');
 			return submittable;
