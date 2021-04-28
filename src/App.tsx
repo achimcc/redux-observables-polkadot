706d82ba36f7022f710d70119598ff2ba762187f
achimcc <achim@parity.com>
2021-04-28 11:44:00 +0200

    cleanup further

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import Instantiate from './components/Instantiate';
@@ -13,18 +11,6 @@ function App() {
 				<header className="App-header">
 					<Instantiate />
 					<Deploy />
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.tsx</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
 				</header>
 			</div>
 		</Provider>
